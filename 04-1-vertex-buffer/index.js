1f60d22b37f31fba4b391ce6750d94d45d6090c5
Nikolai Paramonov <paramonov.work@gmail.com>
2024-06-22 15:21:44 +0200

    feature/04-1: Implement vertex buffer sample

--- 04-1-vertex-buffer/index.js
+++ 04-1-vertex-buffer/index.js
@@ -31,21 +31,19 @@ async function main() {
         scale: vec2f,
       }
       
-      @group(0) @binding(0) var<storage, read> ourStruct: array<OurStruct>;
-      @group(0) @binding(1) var<storage, read> otherStruct: array<OtherStruct>;
-      @group(0) @binding(2) var<storage, read> pos: array<vec2f>;
+      struct Vertex {
+        @location(0) position: vec2f,
+        @location(1) color: vec4f,
+        @location(2) offset: vec2f,
+        @location(3) scale: vec2f,
+        @location(4) perVertexColor: vec3f,
+      }
     
       @vertex 
-      fn vs(
-        @builtin(vertex_index) vertexIndex: u32,
-        @builtin(instance_index) instanceIndex: u32
-      ) -> VSOutput {
-        let otherStruct = otherStruct[instanceIndex];
-        let ourStruct = ourStruct[instanceIndex];
-        
+      fn vs(vert: Vertex) -> VSOutput {
         var vsOut: VSOutput;
-        vsOut.position = vec4f(pos[vertexIndex] * otherStruct.scale + ourStruct.offset, 0.0, 1.0);
-        vsOut.color = ourStruct.color;
+        vsOut.position = vec4f(vert.position * vert.scale + vert.offset, 0.0, 1.0);
+        vsOut.color = vert.color * vec4f(vert.perVertexColor, 1);
         return vsOut;
       }
       
@@ -62,6 +60,30 @@ async function main() {
     vertex: {
       module,
       entryPoint: 'vs',
+      buffers: [
+        {
+          arrayStride: 2 * 4 + 4,
+          attributes: [
+            { shaderLocation: 0, offset: 0, format: 'float32x2' },
+            { shaderLocation: 4, offset: 8, format: 'unorm8x4' },
+          ],
+        },
+        {
+          arrayStride: 4 + 2 * 4,
+          stepMode: 'instance',
+          attributes: [
+            { shaderLocation: 1, offset: 0, format: 'unorm8x4' },   // color
+            { shaderLocation: 2, offset: 4, format: 'float32x2' },  // offset
+          ],
+        },
+        {
+          arrayStride: 2 * 4,
+          stepMode: 'instance',
+          attributes: [
+            { shaderLocation: 3, offset: 0, format: 'float32x2' },   // scale
+          ],
+        },
+      ]
     },
     fragment: {
       module,
@@ -70,77 +92,78 @@ async function main() {
     },
   });
 
-  const staticUnitSize =
-    4 * 4 + // color is 4 32bit floats (4bytes each)
-    2 * 4 + // offset is 2 32bit floats (4bytes each)
-    2 * 4;  // padding
-  const changingUnitSize =
-    2 * 4;  // scale is 2 32bit floats (4bytes each)
-
   const kNumObjects = 100;
   const objectInfos = [];
 
-  const staticStorageBufferSize = staticUnitSize * kNumObjects;
-  const changingStorageBufferSize = changingUnitSize * kNumObjects;
+  const staticUnitSize =
+    4 +     // color is 4 bytes
+    2 * 4;  // offset is 2 32bit floats (4bytes each)
+  const changingUnitSize =
+    2 * 4;  // scale is 2 32bit floats (4bytes each)
+  const staticVertexBufferSize = staticUnitSize * kNumObjects;
+  const changingVertexBufferSize = changingUnitSize * kNumObjects;
 
-  const staticStorageBuffer = device.createBuffer({
-    label: 'static storage for objects',
-    size: staticStorageBufferSize,
-    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+  const staticVertexBuffer = device.createBuffer({
+    label: 'static vertex for objects',
+    size: staticVertexBufferSize,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
   });
 
-  const changingStorageBuffer = device.createBuffer({
-    label: 'changing storage for objects',
-    size: staticStorageBufferSize,
-    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+  const changingVertexBuffer = device.createBuffer({
+    label: 'changing vertex for objects',
+    size: changingVertexBufferSize,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
   });
 
 
   const kColorOffset = 0;
-  const kOffsetOffset = 4;
+  const kOffsetOffset = 1;
   const kScaleOffset = 0;
 
   {
-    const staticStorageValues = new Float32Array(staticStorageBufferSize / 4);
+    const staticVertexValuesU8 = new Uint8Array(staticVertexBufferSize);
+    const staticVertexValuesF32 = new Float32Array(staticVertexValuesU8.buffer);
     for (let i = 0; i < kNumObjects; ++i) {
-      const staticOffset = i * (staticUnitSize / 4);
+      const staticOffsetU8 = i * staticUnitSize;
+      const staticOffsetF32 = staticOffsetU8 / 4;
 
       // These are only set once so set them now
-      staticStorageValues.set([rand(), rand(), rand(), 1], staticOffset + kColorOffset);        // set the color
-      staticStorageValues.set([rand(-0.9, 0.9), rand(-0.9, 0.9)], staticOffset + kOffsetOffset);      // set the offset
+      staticVertexValuesU8.set(        // set the color
+        [rand() * 255, rand() * 255, rand() * 255, 255],
+        staticOffsetU8 + kColorOffset);
+
+      staticVertexValuesF32.set(      // set the offset
+        [rand(-0.9, 0.9), rand(-0.9, 0.9)],
+        staticOffsetF32 + kOffsetOffset);
 
       objectInfos.push({
         scale: rand(0.2, 0.5),
       });
     }
-    device.queue.writeBuffer(staticStorageBuffer, 0, staticStorageValues);
+    device.queue.writeBuffer(staticVertexBuffer, 0, staticVertexValuesF32);
   }
 
-  const storageValues = new Float32Array(changingStorageBufferSize / 4);
+  const storageValues = new Float32Array(changingVertexBufferSize / 4);
 
-  const { vertexData, numVertices } = createCircleVertices({
+  const { vertexData, indexData, numVertices } = createCircleVertices({
     radius: 0.5,
     innerRadius: 0.25,
   });
 
-  const vertexStorageBuffer = device.createBuffer({
-    label: 'storage buffer vertices',
+
+  const vertexBuffer = device.createBuffer({
+    label: 'vertex buffer vertices',
     size: vertexData.byteLength,
-    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
   });
+  device.queue.writeBuffer(vertexBuffer, 0, vertexData);
 
-  device.queue.writeBuffer(vertexStorageBuffer, 0, vertexData);
-
-  const bindGroup = device.createBindGroup({
-    label: 'bind group for objects',
-    layout: pipeline.getBindGroupLayout(0),
-    entries: [
-      { binding: 0, resource: { buffer: staticStorageBuffer }},
-      { binding: 1, resource: { buffer: changingStorageBuffer }},
-      { binding: 2, resource: { buffer: vertexStorageBuffer }},
-    ],
+  const indexBuffer = device.createBuffer({
+    label: 'index buffer',
+    size: indexData.byteLength,
+    usage: GPUBufferUsage.INDEX | GPUBufferUsage.COPY_DST,
   });
-
+  device.queue.writeBuffer(indexBuffer, 0, indexData);
 
   const renderPassDescriptor = {
     label: 'our basic canvas renderPass',
@@ -165,51 +188,63 @@ async function main() {
   observer.observe(canvas);
 
   function createCircleVertices({
-                                  radius = 1,
-                                  numSubdivisions = 24,
-                                  innerRadius = 0,
-                                  startAngle = 0,
-                                  endAngle = Math.PI * 2,
-                                } = {}) {
+    radius = 1,
+    numSubdivisions = 24,
+    innerRadius = 0,
+    startAngle = 0,
+    endAngle = Math.PI * 2,
+  } = {}) {
+    const numVertices = (numSubdivisions + 1) * 2;
     // 2 triangles per subdivision, 3 verts per tri, 2 values (xy) each.
-    const numVertices = numSubdivisions * 3 * 2;
-    const vertexData = new Float32Array(numSubdivisions * 2 * 3 * 2);
+    const vertexData = new Float32Array(numVertices * (2 + 1));
+    const colorData = new Uint8Array(vertexData.buffer);
 
     let offset = 0;
-    const addVertex = (x, y) => {
+    let colorOffset = 8;
+    const addVertex = (x, y, r, g, b) => {
       vertexData[offset++] = x;
       vertexData[offset++] = y;
+      offset += 1;  // skip the color
+      colorData[colorOffset++] = r * 255;
+      colorData[colorOffset++] = g * 255;
+      colorData[colorOffset++] = b * 255;
+      colorOffset += 9;  // skip extra byte and the position
     };
 
-    // 2 triangles per subdivision
-    //
-    // 0--1 4
-    // | / /|
-    // |/ / |
-    // 2 3--5
-    for (let i = 0; i < numSubdivisions; ++i) {
-      const angle1 = startAngle + (i + 0) * (endAngle - startAngle) / numSubdivisions;
-      const angle2 = startAngle + (i + 1) * (endAngle - startAngle) / numSubdivisions;
+    const innerColor = [1, 1, 1];
+    const outerColor = [0.1, 0.1, 0.1];
 
-      const c1 = Math.cos(angle1);
-      const s1 = Math.sin(angle1);
-      const c2 = Math.cos(angle2);
-      const s2 = Math.sin(angle2);
+    for (let i = 0; i <= numSubdivisions; ++i) {
+      const angle = startAngle + (i + 0) * (endAngle - startAngle) / numSubdivisions;
+
+      const c1 = Math.cos(angle);
+      const s1 = Math.sin(angle);
+
+      addVertex(c1 * radius, s1 * radius, ...outerColor);
+      addVertex(c1 * innerRadius, s1 * innerRadius, ...innerColor);
+    }
+
+    const indexData = new Uint32Array(numSubdivisions * 6);
+    let ndx = 0;
+
+    for (let i = 0; i < numSubdivisions; ++i) {
+      const ndxOffset = i * 2;
 
       // first triangle
-      addVertex(c1 * radius, s1 * radius);
-      addVertex(c2 * radius, s2 * radius);
-      addVertex(c1 * innerRadius, s1 * innerRadius);
+      indexData[ndx++] = ndxOffset;
+      indexData[ndx++] = ndxOffset + 1;
+      indexData[ndx++] = ndxOffset + 2;
 
       // second triangle
-      addVertex(c1 * innerRadius, s1 * innerRadius);
-      addVertex(c2 * radius, s2 * radius);
-      addVertex(c2 * innerRadius, s2 * innerRadius);
+      indexData[ndx++] = ndxOffset + 2;
+      indexData[ndx++] = ndxOffset + 1;
+      indexData[ndx++] = ndxOffset + 3;
     }
 
     return {
       vertexData,
-      numVertices,
+      indexData,
+      numVertices: indexData.length,
     };
   }
 
@@ -236,14 +271,17 @@ async function main() {
     const aspect = canvas.width / canvas.height;
 
     pass.setPipeline(pipeline);
+    pass.setVertexBuffer(0, vertexBuffer);
+    pass.setVertexBuffer(1, staticVertexBuffer);
+    pass.setVertexBuffer(2, changingVertexBuffer);
+    pass.setIndexBuffer(indexBuffer, 'uint32');
 
     objectInfos.forEach(({ scale }, ndx) => {
       const offset = ndx * (changingUnitSize / 4);
       storageValues.set([scale / aspect, scale], offset + kScaleOffset);
     });
-    device.queue.writeBuffer(changingStorageBuffer, 0, storageValues);
-    pass.setBindGroup(0, bindGroup);
-    pass.draw(numVertices, kNumObjects);
+    device.queue.writeBuffer(changingVertexBuffer, 0, storageValues);
+    pass.drawIndexed(numVertices, kNumObjects);
     pass.end();
     const commandBuffer = encoder.finish();
     device.queue.submit([commandBuffer]);
